2c388a231ef545c59c8c1bdb3a9d7dc96c225d87
hyacinth237 <acast494@ucr.edu>
2025-04-19 16:21:07 -0700

    Created first portion of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex font-nunito text-blue-600 text-6xl">
+        Color for Kids
+      </div>
+    </div>
+  );
 };
 
 export default Home;
